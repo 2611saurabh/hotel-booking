cc81a7df71915223dba9df2631a799e8567870c7
moodbloom35 <moodbloom35@gmail.com>
2025-05-18 19:10:45 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import { Route, useLocation,Routes } from 'react-router-dom'
+import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+
+const App = () => {
+
+  //this path in localhost if that /owner present nav disapper because
+  //we put terneary opertor in navbar
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar/>}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/rooms' element={<AllRooms/>} />
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
